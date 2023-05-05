d8414337013fd5d7c152cdbe18df8c7810cc4e0f
Sebastien Tourbier <sebastien.tourbier1@gmail.com>
2023-05-05 17:15:27 +0200

    refactor: review implementation of plotting function for the embeddings

--- mip_dmp/plot/embedding.py
+++ mip_dmp/plot/embedding.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -28,77 +29,100 @@ sns.set(
 
 
 def scatterplot_embeddings(
-    fig: plt.Figure,
-    inputDataset: pd.DataFrame,
-    targetCDEs: pd.DataFrame,
-    embedding_method: str = "chars2vec",
-    dim_reduction_method: str = "tsne",
+    fig: plt.Figure, embeddings: dict, matchedCdeCodes: dict, selectedColumnName: str
 ):
-    """Plot word embedding.
+    """Plot the embeddings of the selected column name and CDE codes in a 3D scatter plot.
 
-    Parameters
-    ----------
-    fig: plt.Figure
-        Matplotlib figure to plot
+    fig: matplotlib.figure.Figure
+        Figure to render the 3D scatter plot of the embeddings.
 
-    inputDataset : pd.DataFrame
-        Input dataset.
+    embeddings: dict
+        Dictionary of embeddings in the form::
 
-    targetCDEs : pd.DataFrame
-        Target CDEs.
+            {
+                "x": [5, ..., 2],
+                "y": [0.5, ..., 0.2],
+                "z": [0.5, ..., 0.2],
+                "label": ["word1", ..., "wordN"],
+                "type": ["cde", ..., "column"]
+            }
 
-    embedding_method : str
-        Embedding method. Can be "chars2vec" or "glove".
-        Default: "chars2vec".
+        where `x`, `y` and `z` are the lists of the x, y and z coordinates of the embeddings,
+        `label` is the list of the labels of the embeddings and `type` is the list of the
+        types of the embeddings (can be "column" or "cde").
 
-    dim_reduction_method : str
-        Dimensionality reduction method. Can be "tsne" or "pca".
+    matchedCdeCodes: dict
+        Dictionary of the matched CDE codes in the form::
 
-    Returns
-    -------
-    fig : plt.figure
-        Figure.
+            {
+                "input_dataset_column1": {
+                    "words": ["cde_code1", "cde_code2", ...],
+                    "embeddings": [embedding_vector1, embedding_vector2, ...]
+                    "distances": [distance1, distance2, ...]
+                },
+                "input_dataset_column2": {
+                    "words": ["cde_code1", "cde_code2", ...],
+                    "embeddings": [embedding_vector1, embedding_vector2, ...]
+                    "distances": [distance1, distance2, ...]
+                },
+                ...
+            }
+
+    selectedColumnName: str
+        Name of the selected column.
     """
-    # Extract list of columns and CDE codes
-    columns = inputDataset.columns
-    cde_codes = targetCDEs["code"].unique()
-    # Print info about method's inputs
-    print(
-        f"> Generating embeddings for {len(columns)} columns and {len(cde_codes)} CDE codes..."
-    )
-    print(f"  - Embedding method: {embedding_method}")
-    # Generate embeddings
-    column_embeddings = generate_embeddings(columns, embedding_method)
-    cde_embeddings = generate_embeddings(cde_codes, embedding_method)
-    # Reduce embeddings dimension to 3 components via t-SNE or PCA for visualization
-    x_column, y_column, z_column = reduce_embeddings_dimension(
-        column_embeddings, reduce_method=dim_reduction_method
-    )
-    x_cde, y_cde, z_cde = reduce_embeddings_dimension(
-        cde_embeddings, reduce_method=dim_reduction_method
-    )
+    print("> Generate scatterplot...")
+    # Get the words for which their embeddings have been matched to column name
+    selected_column_matches = matchedCdeCodes[selectedColumnName]["words"]
+    # Generate filtered list of embeddings
+    filtered_embeddings = {}
+    for key in embeddings.keys():
+        filtered_embeddings[key] = [
+            embeddings[key][i]
+            for i, t in enumerate(embeddings["type"])
+            if t == "cde" and embeddings["label"][i] in selected_column_matches
+        ] + [
+            embeddings[key][i]
+            for i, t in enumerate(embeddings["type"])
+            if t == "column" and embeddings["label"][i] == selectedColumnName
+        ]
+
+    # Determine a scaled dynamic jittering for the scatter plot based on
+    # the dynamic range of the data in the given dimension
+    def rand_jitter(arr, scale=0.3):
+        """Return random noise for jittering."""
+        stdev = scale * (max(arr) - min(arr))
+        return stdev
+
+    stdev_x = rand_jitter(filtered_embeddings["x"])
+    stdev_y = rand_jitter(filtered_embeddings["y"])
+    stdev_z = rand_jitter(filtered_embeddings["z"])
     # Format data as pandas dataframe for plotting
     df = pd.DataFrame(
         {
-            "x": list(x_column) + list(x_cde),
-            "y": list(y_column) + list(y_cde),
-            "z": list(z_column) + list(z_cde),
-            "label": list(columns) + list(cde_codes),
-            "type": ["column"] * len(x_column) + ["cde"] * len(x_column),
+            "x": filtered_embeddings["x"],
+            "y": filtered_embeddings["y"],
+            "z": filtered_embeddings["z"],
+            "label": filtered_embeddings["label"],
+            "type": filtered_embeddings["type"],
         }
     )
-    print(df)
     # Create a scatter plot with different colors for each group
     ax = fig.add_subplot(111, projection="3d")
+    artists = {}
+    annotation_texts = {}
     for t, color in zip(df["type"].unique(), COLORS):
-        ax.scatter(
+        artists[t] = ax.scatter(
             df.loc[df["type"] == t, "x"],
             df.loc[df["type"] == t, "y"],
             df.loc[df["type"] == t, "z"],
             color=color,
             label=t,
             picker=True,
+            alpha=0.5,
+            s=50,
         )
+        annotation_texts[t] = []
     # Set labels and show legend
     ax.set_xlabel("X")
     ax.set_ylabel("Y")
@@ -111,54 +135,65 @@ def scatterplot_embeddings(
 
         def handle_annotations(artist, indices):
             """Add text annotations to closest point of the cursor when the mouse button was pressed."""
-            texts = []
+            # Get the type of the artist that can be "cde" or "column"
+            for k in artists.keys():
+                if artist == artists[k]:
+                    artist_type = k
+            artist_df = df[df["type"] == artist_type]
+            # For each index of the artist
             for ind in indices:
+                # Get the coordinates of the mouse click
                 points_x, points_y, points_z = artist._offsets3d
                 (x, y, z) = (
                     points_x[ind].__float__(),
                     points_y[ind].__float__(),
                     points_z[ind].__float__(),
                 )
-                # x, y, z = get_xyz_mouse_click(event, ax)
                 # Find the closest point in the dataframe
                 idx = (
-                    (df["x"] - x) ** 2 + (df["y"] - y) ** 2 + (df["z"] - z) ** 2
+                    (artist_df["x"] - x) ** 2
+                    + (artist_df["y"] - y) ** 2
+                    + (artist_df["z"] - z) ** 2
                 ).idxmin()
-                # Get the value of src for the selected point
-                val = df.loc[idx, "type"]
-                # create the annotation text based on the value of src
-                text = f"{df.loc[idx, 'label']}"
-                print(f"Mouse click at ({x}, {y}, {z}) for label {text}")
-                # Add jitter for overlapping points if needed
-                # (x, y, z) = (x, y, z)
+                # Get the corresponding label
+                text = f"{artist_df.loc[idx, 'label']}"
+                # print(
+                #     f"Mouse click at ({x}, {y}, {z}) for label {text} ({artist_type})"
+                # )
+                # Handle the removal of the annotation if it already exists and is clicked again
+                abord = False
+                for annotation in ax.texts:
+                    annotation_text = annotation.get_text()
+                    if (annotation_text == text) and (
+                        text in annotation_texts[artist_type]
+                    ):
+                        annotation.remove()
+                        annotation_texts[artist_type].remove(text)
+                        abord = True
+                if abord:
+                    return
+                # Add a random jittering to the coordinates of the annotation for the "column" type.
+                # Useful to avoid overlapping of the annotations when the points are overlapping.
+                if artist_type == "column":
+                    stdev = np.min([stdev_x, stdev_y, stdev_z])
+                    np.random.seed(42)
+                    x += np.random.randn() * stdev
+                    y += np.random.randn() * stdev
+                    z += np.random.randn() * stdev
+                # print(f"Text shown at ({x}, {y}, {z}) for label {text}")
                 # Add the annotation to the plot with the selected color based on type
-                if val == "column":
-                    ax.text(
-                        x,
-                        y,
-                        z,
-                        text,
-                        fontsize=10,
-                        color=COLORS[0],
-                        backgroundcolor="#2222",
-                        fontweight="bold",
-                    )
-                elif val == "cde":
-                    ax.text(
-                        x,
-                        y,
-                        z,
-                        text,
-                        fontsize=10,
-                        color=COLORS[1],
-                        backgroundcolor="#2222",
-                        fontweight="bold",
-                    )
-                texts.append(text)
-            # remove previous annotations
-            for annotation in ax.texts:
-                if annotation.get_text() not in texts:
-                    annotation.remove()
+                ax.text(
+                    x,
+                    y,
+                    z,
+                    text,
+                    fontsize=10,
+                    color=COLORS[1] if artist_type == "column" else COLORS[0],
+                    backgroundcolor="#081512",
+                    fontweight="bold",
+                )
+                # Add the annotation to the list of annotations
+                annotation_texts[artist_type].append(text)
 
         # Get the coordinates of the point clicked
         handle_annotations(event.artist, event.ind)
