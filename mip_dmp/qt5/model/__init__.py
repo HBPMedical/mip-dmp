9fd82068cffdc34c8620bcb7d37e5ae611c39096
Sébastien Tourbier <sebastien.tourbier1@gmail.com>
2023-08-10 13:39:03 +0200

    docs: add header for license in all .py files and add an how to cite section in README

--- mip_dmp/qt5/model/__init__.py
+++ mip_dmp/qt5/model/__init__.py
@@ -1,1 +1,15 @@
-"""Sub-package for PySide2 models."""
+# Copyright 2023 The HIP team, University Hospital of Lausanne (CHUV), Switzerland & Contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Sub-package for PySide2 tabular data models of the MIP Dataset Mapper UI application."""
--- mip_dmp/qt5/model/table_model.py
+++ mip_dmp/qt5/model/table_model.py
@@ -1,4 +1,18 @@
-"""Module to describe Qt Table Models."""
+# Copyright 2023 The HIP team, University Hospital of Lausanne (CHUV), Switzerland & Contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Module to describe Qt/PySide2 Table Models."""
 
 from PySide2 import QtCore
 from PySide2.QtCore import Qt
